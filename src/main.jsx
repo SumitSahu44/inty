22ac35a770393853f322582939dcd96b3b9ecd54
Sumit Sahu <99080079+SumitSahu44@users.noreply.github.com>
2025-07-02 23:34:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,6 @@ const clerk_key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!clerk_key) {
   throw new Error("Clerk Publishable Key was not found.");
 }
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ClerkProvider 
